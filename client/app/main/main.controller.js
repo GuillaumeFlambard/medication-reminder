39d87f1f238ea68243700ebba8c682b3ebf35eca
Guillaume Flambard <guillaume.flambard01@gmail.com>
2017-09-14 22:08:26 -0400

    Implementation of complet medication action

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -13,4 +13,16 @@ angular.module('medicationReminderApp').controller('MainCtrl', function ($scope,
         $scope.currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
         $scope.$apply();
     }, 1000);
+
+    /**
+     * @param medication
+     *
+     * To complete a medication, passe the boolean to true
+     */
+    $scope.completeMedication = function(medication) {
+        //Remove useless key
+        delete medication['$$hashKey'];
+        medication.completed = true;
+        $http.put('/api/medications/' + medication._id, JSON.stringify(medication));
+    };
 });
