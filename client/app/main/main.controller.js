864449ab0a5d869bfe95481663d7ff8ddc79e227
Guillaume Flambard <guillaume.flambard01@gmail.com>
2017-09-16 10:22:46 -0400

    Manage the 2 next medication signals and reset next medication after

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -35,10 +35,20 @@ angular.module('medicationReminderApp').controller('MainCtrl', function ($scope,
         //Remove useless key
         delete medication['$$hashKey'];
         medication.completed = true;
-        $http.put('/api/medications/' + medication._id, JSON.stringify(medication));
+        $http.put('/api/medications/' + medication._id, JSON.stringify(medication)).then(function (response) {
+            if (response.status == 200)
+            {
+                //If this medication is the name medication we have to reset the next medication
+                if ($scope.nextMed && response.data._id == $scope.nextMed._id)
+                {
+                    $scope.nextMed = null;
+                }
+            }
+        });
     };
 
     function managedNextMedication(currentTime) {
+        //Set nextMed if nextMed is null
         if (!$scope.nextMed)
         {
             $http.get('/api/medications/next_medication').then(function (nextMed) {
@@ -48,15 +58,19 @@ angular.module('medicationReminderApp').controller('MainCtrl', function ($scope,
 
         if ($scope.nextMed)
         {
-            var nextMedicationTime = new Date($scope.nextMed.time);
-            console.log(nextMedicationTime, currentTime._d);
-            console.log(nextMedicationTime == currentTime._d);
-            console.log(nextMedicationTime == currentTime);
+            var nextMedicationTime = moment($scope.nextMed.time);
 
-            if (typeof $scope.nextMed != "undefined" && nextMedicationTime == currentTime) {
+            if (typeof $scope.nextMed != "undefined" && currentTime.diff(nextMedicationTime) >= 0 && currentTime.diff(nextMedicationTime) < 1000) {
                 console.log('Alert 1');
-            } else if (typeof $scope.nextMed != "undefined" && nextMedicationTime == currentTime.subtract(5, 'minute')) {
-                console.log('Alert 2');
+            } else {
+                nextMedicationTime.add(5, 'minute');
+                if (typeof $scope.nextMed != "undefined" && currentTime.diff(nextMedicationTime) >= 0 && currentTime.diff(nextMedicationTime) < 1000) {
+                    console.log('Alert 2');
+                    //put this medication in the mist medication list
+                    $scope.mistMeds.push($scope.nextMed);
+                    //Set nextMed to null to reset next med
+                    $scope.nextMed = null;
+                }
             }
         }
     }
